15c9e9957308ef4ada6e8727a331487b5e066d8b
DevendraSosa <devendrasosa111@gmail.com>
2022-11-03 15:25:26 +0530

    D

---
+++ Assignment_1.py
@@ -0,0 +1,74 @@
+#  1 .Triple all the numbers given in list
+
+# nums = (1,2,3,4,5,6,)
+
+# result = list(map(lambda i : i + i + i , nums))
+
+# print (result)
+
+
+#  2 . Separate even odd number from given list
+
+# def split(Even_odd):
+#     ev_li = []
+#     od_li = []
+
+#     for i in Even_odd:
+#         if (i % 2 == 0):
+#             ev_li.append(i)
+        
+#         else :
+#             od_li.append(i)
+    
+#     print("Even lists :" , ev_li)
+#     print("odd lists :",od_li)
+
+# Even_odd = list()
+# n=int(input("enter the size of the list : " ))
+# print("enter the element of first list : " )
+
+# for i in range(int(n)):
+#     a = int(input(""))
+#     Even_odd.append(a)
+# split(Even_odd)
+
+# ...............................................................................................
+
+# 3 . Print all string in lower case from given tuple
+
+# def lower(i):
+#     return i.lower()
+
+# tuple_example = ("MY","NAME","IS","___")
+
+# upadate_tuple = tuple(map(lower,tuple_example))
+
+# print(upadate_tuple)
+
+# 4 . Print square root of numbers given in list
+
+# import math
+# from re import X
+# from turtle import update
+# def square_root(x):
+#     return math.sqrt(x)
+
+# num = (2,3,4,5)
+
+# upa_list = map(square_root,num)
+
+# print(list(upa_list))
+
+# 5 . Eliminate duplicate letter from given string
+
+# def eliminate_duplicate(x):
+#     letters = set(a)
+#     letters = "".join(letters)
+#     print("before elimnate duplicate letters :",a)
+#     print ("after elimnate duplicate letters :",letters)
+
+# a="evendra"
+# eliminate_duplicate (a)
+
+
+
